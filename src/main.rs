99fd017744682127677b47e7ab726f3c36847594
Jess Frazelle <github@jessfraz.com>
2023-10-19 11:11:41 -0700

    updates
    Signed-off-by: Jess Frazelle <github@jessfraz.com>

--- src/main.rs
+++ src/main.rs
@@ -18,8 +18,8 @@ use serenity::{
     client::bridge::gateway::{ShardId, ShardManager},
     framework::standard::{
         help_commands,
-        macros::{check, command, group, help, hook},
-        Args, CommandGroup, CommandOptions, CommandResult, DispatchError, HelpOptions, Reason, StandardFramework,
+        macros::{command, group, help, hook},
+        Args, CommandGroup, CommandResult, DispatchError, HelpOptions, StandardFramework,
     },
     http::Http,
     model::{
@@ -161,13 +161,14 @@ impl EventHandler for Handler {
 }
 
 #[group]
-#[commands(about, design, latency, ping)]
+#[commands(about)]
 struct General;
 
 #[group]
 #[owners_only]
 // Limit all commands to be guild-restricted.
 #[only_in(guilds)]
+#[commands(design, latency, ping)]
 // Summary only appears when listing multiple groups.
 #[summary = "Commands for server owners"]
 struct Owner;
@@ -318,6 +319,7 @@ async fn run_cmd(opts: &Opts) -> Result<()> {
             } else {
                 owners.insert(info.owner.id);
             }
+            slog::info!(crate::LOGGER, "Got owners: {:?}", owners);
             let bot_id = http.get_current_user().await?;
 
             // Set up the framework.
@@ -397,34 +399,6 @@ async fn run_cmd(opts: &Opts) -> Result<()> {
     Ok(())
 }
 
-// A function which acts as a "check", to determine whether to call a command.
-//
-// In this case, this command checks to ensure you are the owner of the message
-// in order for the command to be executed. If the check fails, the command is
-// not called.
-#[check]
-#[name = "Owner"]
-async fn owner_check(_: &Context, msg: &Message, _: &mut Args, _: &CommandOptions) -> Result<(), Reason> {
-    // Replace 7 with your ID to make this check pass.
-    //
-    // 1. If you want to pass a reason alongside failure you can do:
-    // `Reason::User("Lacked admin permission.".to_string())`,
-    //
-    // 2. If you want to mark it as something you want to log only:
-    // `Reason::Log("User lacked admin permission.".to_string())`,
-    //
-    // 3. If the check's failure origin is unknown you can mark it as such:
-    // `Reason::Unknown`
-    //
-    // 4. If you want log for your system and for the user, use:
-    // `Reason::UserAndLog { user, log }`
-    if msg.author.id != 7 {
-        return Err(Reason::User("Lacked owner permission".to_string()));
-    }
-
-    Ok(())
-}
-
 #[command]
 async fn about(ctx: &Context, msg: &Message) -> CommandResult {
     msg.reply(
@@ -439,7 +413,6 @@ async fn about(ctx: &Context, msg: &Message) -> CommandResult {
 #[command]
 // Limit command usage to guilds.
 #[only_in(guilds)]
-#[checks(Owner)]
 async fn latency(ctx: &Context, msg: &Message) -> CommandResult {
     // The shard manager is an interface for mutating, stopping, restarting, and
     // retrieving information about shards.
@@ -478,7 +451,6 @@ async fn latency(ctx: &Context, msg: &Message) -> CommandResult {
 #[command]
 // Limit command usage to guilds.
 #[only_in(guilds)]
-#[checks(Owner)]
 async fn ping(ctx: &Context, msg: &Message) -> CommandResult {
     msg.reply(&ctx.http, "Pong! : )").await?;
 
@@ -487,7 +459,6 @@ async fn ping(ctx: &Context, msg: &Message) -> CommandResult {
 
 #[command]
 #[description = "Generate a CAD model from a text prompt."]
-#[checks(Owner)]
 async fn design(ctx: &Context, msg: &Message, mut args: Args) -> CommandResult {
     match args.single_quoted::<String>() {
         Ok(x) => {
